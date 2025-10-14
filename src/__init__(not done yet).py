b16efbca9f56ccf63bfbd24350c32de0529a8d65
AnastasiaRassi <anastasiarassi2@gmail.com>
2025-10-14 15:48:31 +0300

    fixing config.yaml

--- src/__init__(not done yet).py
+++ src/__init__(not done yet).py
@@ -1,8 +1,3 @@
 # to  import directly from src.py
 # relative imports (within the same package)
-from .preprocess import preprocess_data
-from .train import train_model
-from .evaluate import evaluate_model
-from .utils import load_data, save_model
-from .utils.preprocessing_utils import PreprocessingUtils
 from .logger_config import setup_logger
